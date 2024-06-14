1d3fe397065a15b68bd6aa06e52a1a251b8984e0
lewin349 <lewinl349@gmail.com>
2024-06-14 19:53:06 -0400

    Add base player with "wasd" movement
    Added basic movement for the player and placeholder sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,21 @@
-extends Node
+extends CharacterBody2D
 
+const SPEED = 100.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	# Get directionv values
+	var directionX = Input.get_axis("move_left", "move_right")
+	var directionY = Input.get_axis("move_up", "move_down")
+	
+	# Apply movement	
+	if directionX:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	move_and_slide()
